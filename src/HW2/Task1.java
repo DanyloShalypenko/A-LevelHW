df2b46c84b82f0b52b67f538f090cbaad931e279
DanyloShalypenko <anybic1103@gmail.com>
2023-09-30 19:30:47 +0300

    Task 3 HW2

--- src/HW2/Task1.java
+++ src/HW2/Task1.java
@@ -1,2 +1,17 @@
-package HW2;public class Task1 {
-}
+package HW2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double  x1 = 1.0, x2 = 3.0, x3 = 6.0;
+        double y1 = 5.0, y2 = 5.0, y3 = 7.0;
+
+        double square = calculateSquare(x1, y1, x2, y2, x3, y3);
+            System.out.println("Площадь: " + square);
+        }
+
+
+    public static double calculateSquare(double x1, double y1, double x2, double y2, double x3, double y3) {
+
+        return Math.abs((x1 - x3)*(y2 - y3) - (x2 - x3)*(y1 - y3)/2);
+        }
+    }
--- src/HW2/Task4.java
+++ src/HW2/Task4.java
@@ -1,2 +1,25 @@
-package HW2;public class Task4 {
+package HW2;
+
+public class Task4 {
+    public static void main(String[] args) {
+        double num1 = 5.0;
+        double num2 = -3.0;
+        double num3 = 7.0;
+
+        printSmallestValue(num1, num2, num3);
+    }
+
+    public static void printSmallestValue(double num1, double num2, double num3) {
+
+        double absNum1 = num1 < 0 ? -num1 : num1;
+        double absNum2 = num2 < 0 ? -num2 : num2;
+        double absNum3 = num3 < 0 ? -num3 : num3;
+
+        double smallestAbsValue = (absNum1 <= absNum2 && absNum1 <= absNum3) ? num1 :
+                (absNum2 <= absNum1 && absNum2 <= absNum3) ? num2 :
+                        num3;
+
+
+        System.out.println("Меньшее по модулю число: " + smallestAbsValue);
+    }
 }
