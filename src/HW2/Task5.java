a9803e0a329d1f4b131f99fe4dbc06dde2a828ac
DanyloShalypenko <anybic1103@gmail.com>
2023-09-30 20:56:54 +0300

    Add 2-nd method to Task 5

--- src/HW2/Task5.java
+++ src/HW2/Task5.java
@@ -7,12 +7,13 @@ public class Task5 {
         while (true) { // Цыкл, бесконечный потому что лень перезапускать для проверки.
             Scanner klog = new Scanner(System.in);
             System.out.print("Write your nubmer: ");
-            int num = klog.nextInt(); // 1-st method
+            int num = klog.nextInt();
 
-            System.out.println(reverse(num));
+          //System.out.println(reverseWithInt(num)); 1-st method
+            System.out.println(reverseWithString(num)); // 2-nd method
         }
     }
- public static int reverse (int num) {
+ public static int reverseWithInt (int num) {
         int reversenum = 0;
         while (num !=0) {
             reversenum = reversenum*10 + num%10;
@@ -20,4 +21,14 @@ public class Task5 {
         }
         return reversenum; // 1-st method
    }
+    public static String reverseWithString(int num) {
+        String numStr = Integer.toString(num);
+        String reversedNumbStr = new StringBuilder(numStr).reverse().toString();
+
+        if (reversedNumbStr.endsWith("-")) {
+            reversedNumbStr = "-" + reversedNumbStr.substring(0, reversedNumbStr.length() - 1);
+        }
+
+        return reversedNumbStr;
+    } // 2-nd method
 }
