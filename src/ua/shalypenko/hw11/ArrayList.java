40b7180e0e7f3f59242809135f6a1a05f66c3190
DanyloShalypenko <anybic1103@gmail.com>
2023-11-10 23:50:44 +0200

    add hw11

---
+++ src/ua/shalypenko/hw11/ArrayList.java
@@ -0,0 +1,78 @@
+package ua.shalypenko.hw11;
+
+public class ArrayList {
+    private Object[] array;
+    private int size;
+
+    public ArrayList(int size) {
+        this.size = size;
+        array = new Object[size];
+        // Initialize all elements to null
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+    }
+
+    public void add(Object element) {
+        ensureCapacity();
+        // Assign the element to the first available slot
+        for (int i = 0; i < size; i++) {
+            if (array[i] == null) {
+                array[i] = element;
+                break;
+            }
+        }
+    }
+
+    public void add(Object element, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+
+        ensureCapacity();
+
+        // Shift elements to the right to make space for the new element
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[index] = element;
+        size++;
+    }
+
+    public int indexOf(Object element) {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(array[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isContains(Object element) {
+        return indexOf(element) != -1;
+    }
+
+    private void ensureCapacity() {
+        if (size >= array.length) {
+            int newCapacity = (array.length * 3) / 2 + 1;
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return array[index];
+    }
+}
+
+
+
---
+++ src/ua/shalypenko/hw11/Main.java
@@ -0,0 +1,32 @@
+package ua.shalypenko.hw11;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList(3);
+
+        list.add("apple");
+        list.add(42); // int value
+        list.add(213);
+
+        System.out.println("ArrayList after initial addition:");
+        printArrayList(list);
+
+        list.add("date", 3);
+
+        System.out.println("\nArrayList after adding an element at index 1:");
+        printArrayList(list);
+
+        int index = list.indexOf(42);
+        System.out.println("\nIndex of 42: " + index);
+
+        boolean contains = list.isContains("grape");
+        System.out.println("Contains 'grape': " + contains);
+    }
+
+    public static void printArrayList(ArrayList list) {
+        for (int i = 0; i < list.getSize(); i++) {
+            Object element = list.get(i);
+            System.out.println(element);
+        }
+    }
+}
